551bf5400c8598fe7178f04bf891dd2f5ebc4e88
muntashir-wahid <muntashir.wahid@yahoo.com>
2022-06-25 13:19:47 +0600

    done with the header

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./Header.module.css";
+import mealsImg from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = function () {
+  return (
+    <React.Fragment>
+      <header className={styles.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={styles["main-image"]}>
+        <img src={mealsImg} alt="A table full of delicious foods!!" />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Header;
